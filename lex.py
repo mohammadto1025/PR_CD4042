20be61883a0e064a3df8252eb04df16caeebd621
Mohammad <mahdi.tohidinasab@email.kntu.ac.ir>
2026-08-01 14:33:19 +0330

    Initial the Lexer Class

--- lex.py
+++ lex.py
@@ -9,3 +9,29 @@ class Token:
 
     def __repr__(self):
         return f"Token({self.type}, '{self.lexeme}', {self.line}:{self.column})"
+
+class Lexer:
+    def __init__(self, code, filename="<input>"):
+        self.code = code
+        self.filename = filename
+        self.index = 0
+        self.line = 1
+        self.column = 1
+        self.length = len(code)
+
+        self.keywords = {
+            'if', 'else', 'while', 'for', 'return', 'int', 'float', 'char',
+            'void', 'double', 'struct', 'break', 'continue', 'sizeof',
+            'typedef', 'enum', 'union', 'switch', 'case', 'default',
+            'do', 'goto', 'const', 'static', 'extern', 'register',
+            'volatile', 'signed', 'unsigned', 'short', 'long'
+        }
+
+        self.operators = {
+            '<=', '>=', '==', '!=', '&&', '||', '++', '--',
+            '+=', '-=', '*=', '/=', '%=', '->', '::', '...',
+            '<<', '>>', '&', '|', '^', '~', '!', '=',
+            '+', '-', '*', '/', '%', '<', '>', '.'
+        }
+        self.delimiters = {'{', '}', '(', ')', '[', ']', ';', ',', ':'}
+        self.operator_list = sorted(self.operators, key=len, reverse=True)
