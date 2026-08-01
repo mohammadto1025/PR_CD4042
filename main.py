917bca92752d1dd5a1a4174db35a8a85e7e34c3a
Mohammad <mahdi.tohidinasab@email.kntu.ac.ir>
2026-08-01 15:02:03 +0330

    Importing the parser class and writing an example in the main class

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from lex import Lexer  
+from lex import Lexer
+from pars import Parser  
 
 code = """
 int factorial(int n) {
@@ -12,3 +13,13 @@ tokens = lexer.tokenize()
 
 for tok in tokens:
         print(tok)
+
+parser = Parser(tokens)
+ast = parser.parse_program()
+
+print("=" * 50)
+print("AST Result:")
+print(ast)
+print("\nSyntax Errors (if any):")
+for err in parser.errors:
+    print(err)
